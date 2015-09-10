052c2f12f9c2573f9c07b640c897aad3edd5b97f
Rosario Tarabocchia <rtarabocchia@gmail.com>
2015-09-10 11:23:48 -0400

    Initial Commit

--- Navigation Bars/ViewController.swift
+++ Navigation Bars/ViewController.swift
@@ -9,10 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    var timer = NSTimer()
+    
+    var count = 0
+    
+    func results() {
+        
+        count++
+        
+        println(count)
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector ("results"), userInfo: nil, repeats: true)
+        
     }
 
     override func didReceiveMemoryWarning() {
